2d8662581968da4405720d136af4ed1d70f0985d
Max <mzheng1@my.hpu.edu>
2017-12-14 01:56:01 +0000

    part 1 complet

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,10 @@
 class MoviesController < ApplicationController
+  def redirectUrl(method, url)
+    mark = url.index("?")
+    query = url[mark..url.length-1]
+    return method+query
+  end
 
-  # See Section 4.5: Strong Parameters below for an explanation of this method:
-  # http://guides.rubyonrails.org/action_controller_overview.html
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
@@ -12,11 +15,86 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
- def index
-         @movies = Movie.order(params[:sort_by])
-
-         @sort_column = params[:sort_by]
+  def index
+    @all_ratings = Movie.getRatings
+    
+    # check if all checkbox checked
+    if request.original_url == root_path+"?utf8=✓&commit=Refresh"
+      redirect_to session[:previously]
+    end
+    
+    if params[:method] == "title"
+      @action = "title"
+      if params[:ratings]
+        query = []
+        @all_ratings.each do |rating, value|
+          if !params[:ratings].include?(rating)
+            @all_ratings[rating] = false
+          else
+            query.push(rating)
+          end
+        end
+        @movies = Movie.getMoviesByRatingsTitleOrder(query)
+        session[:url] = request.original_url
+        session[:previously] = request.original_url
+      else
+        if session[:url]
+          redirect_to root_path+redirectUrl("title",session[:url])
+        else
+          @movies = Movie.getMoviesByTitleOrder
+          session[:previously] = request.original_url
+        end
       end
+    elsif params[:method] == "date"
+      @action = "date"
+      if params[:ratings]
+        query = []
+        @all_ratings.each do |rating, value|
+          if !params[:ratings].include?(rating)
+            @all_ratings[rating] = false
+          else
+            query.push(rating)
+          end
+        end
+        @movies = Movie.getMoviesByRatingsDateOrder(query)
+        session[:url] = request.original_url
+        session[:previously] = request.original_url
+      else
+        if session[:url]
+          redirect_to root_path+redirectUrl("date",session[:url])
+        else
+          @movies = Movie.getMoviesByDateOrder
+          session[:previously] = request.original_url
+        end
+      end
+    elsif params[:method] == "back"
+      redirect_to session[:previously]
+    else
+      session.clear
+      if params[:ratings]
+        query = []
+        @all_ratings.each do |rating, value|
+          if !params[:ratings].include?(rating)
+            @all_ratings[rating] = false
+          else
+            query.push(rating)
+          end
+        end
+        @movies = Movie.getMoviesByRatings(query)
+        session[:url] = request.original_url
+        session[:previously] = request.original_url
+      else
+        if session[:url]
+          url = session[:url]
+          session.clear
+          redirect_to root_path+redirectUrl("",url)
+        else
+          @movies = Movie.all
+          session[:previously] = request.original_url
+        end
+      end
+    end
+  end
 
   def new
     # default: render 'new' template
@@ -45,7 +123,5 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
-  private :movie_params
   
 end
